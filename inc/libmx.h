cfa134f6934a39c3de35199405ca8544e39b4e87
Volodymyr Aivazovskyi <aivazovskyi.volodymyr@gmail.com>
2020-10-13 21:43:41 +0300

    added atol

--- inc/libmx.h
+++ inc/libmx.h
@@ -110,9 +110,10 @@ int mx_list_size(t_list *list);
 t_list *mx_sort_list(t_list *lst, bool (*cmp)(void *, void *));
 
 /*
- *	OTHER USEFULL FUNCTION - 20 functions
+ *	OTHER USEFULL FUNCTION - 21 functions
  */
 int mx_atoi(const char *str);
+long mx_atol(const char *str);
 int mx_strncmp(const char *s1, const char *s2, int n);
 char *mx_strchr(const char *s, int c);
 bool mx_isspace(char c);
